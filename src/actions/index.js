1ae6294263d8ad2b37a27f472ac025ebe4a5780e
awaisabir <awaisabirqureshi@gmail.com>
2017-08-08 10:50:29 -0600

    action creators

---
+++ src/actions/index.js
@@ -0,0 +1,6 @@
+export function selectBook(book) {
+
+    // selectBook is an ActionCreator that needs to return an object with a type
+    // property
+    return {type: 'BOOK_SELECTED', payload: book};
+}
